6fcf12cd2e4bb66ae9b18e4e0a207f2ad45b3f60
Bruno Araujo <bruharauj@gmail.com>
2023-10-03 21:31:30 +0200

    modifications made

--- danger_alert_rio_de_janeiro_tourists/project_description.mk
+++ danger_alert_rio_de_janeiro_tourists/project_description.mk
@@ -1,12 +1,12 @@
-#Project: Danger Alert to Rio De Janeiro's Tourists
+# Project: Danger Alert to Rio De Janeiro's Tourists
 
-##Introduction
-##Project Overwiew
-##Data Exploration
-##Storytelling
-##Tools and Technologies
-##Data Visualization
-##Results and Impact
-##Challenges and Lessons Learned
-##Acknowledgments 
-##Conclusion
+## Introduction
+## Project Overwiew
+## Data Exploration
+## Storytelling
+## Tools and Technologies
+## Data Visualization
+## Results and Impact
+## Challenges and Lessons Learned
+## Acknowledgments 
+## Conclusion
